4fa1fa8c7b6f5f5e0b5a1e918accea2617423efe
plcsilveira <plcarvalho2010@hotmail.com>
2026-06-24 14:12:09 -0300

    init

--- sistema-consultas/src/main/java/br/edu/ifma/consultas/application/ports/out/ProntuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifma.consultas.application.ports.out;
-
-import br.edu.ifma.consultas.domain.Prontuario;
-
-public interface ProntuarioRepository {
-    void salvar(Prontuario prontuario);
-    Prontuario buscarPorId(Long id);
-    Prontuario buscarPorConsultaId(Long idConsulta);
-}
